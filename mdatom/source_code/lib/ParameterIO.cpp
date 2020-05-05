58e191209e93375c679984770f990e327a1f51d2
wolfartl <wolfartl@student.ethz.ch>
2020-05-05 22:58:04 +0200

    added a parameter for the input file to control type of simulation (with angles etc)

--- mdatom/source_code/lib/ParameterIO.cpp
+++ mdatom/source_code/lib/ParameterIO.cpp
@@ -44,6 +44,7 @@ MDParameters ParameterIO::readParameters(const std::string &fileName) {
     fin.ignore();
     getline(fin, dummy);
     fin >> par.randomSeed;
+		>> par.ChainSimType;
 
     fin.ignore();
     getline(fin, dummy);
