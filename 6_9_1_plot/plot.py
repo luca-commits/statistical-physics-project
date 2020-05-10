Finished subtask 6.9.1. Fixed plot by adjusting y-limit and converting units of constants correctly.

6_9_1_plot/plot.py
@@ -2,25 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-  # For C-O bond
-  D_e = 40100 # 1/nm
-  r_0 = 1.229 # nm
-  a = 2.32 # nm
-  k_r = 570.0 # kcal /(mol A^2)
+  # For C-O bond, values taken from Cornell (1995) paper (p. 5194)and Morse (1929) paper (p. 62)
+  D_e = 91600 # wave no = 1 / cm
+  r_0 = 1.229 # Angström
+  a = 2.29 # 1 / Angström
+  k_r = 12.53e-3 # 1 / (Angström^3)
   
-  r = np.arange(0, 10, 0.1)
-  V_harmonic = 0.5 * k_r * (r - r_0)**2
-  V_morse = D_e * (1 - np.exp(-a * (r - r_0)))**2
+  r = np.arange(0, 5, 0.01)
   
-  plt.plot(r, V_morse)
-  plt.plot(r, V_harmonic)
+  V_harmonic = 0.5 * k_r * (r - r_0)**2 * 10e8 # wave no = 1 / cm
+  V_morse = D_e * (1 - np.exp(-a * (r - r_0)))**2 # wave no = 1 / cm
   
+  plt.plot(r, V_morse, label='Morse potential')
+  plt.plot(r, V_harmonic, label='Harmonic potential')
   
-  plt.title("Comparison of harmonic potential energy function vs. Morse potential energy function")
-  # plt.xlabel()
+  plt.ylim(0, 200000)
   
-  plt.ylim(0, 50000)
-  plt.show()
+  plt.title("Harmonic potential energy function \n vs. Morse potential energy function for C-O bond")
+  plt.xlabel("Nuclear seperation (in Angström)")
+  plt.ylabel("Energy (in wave numbers)")
+  plt.legend()
+  
+  plt.gcf().subplots_adjust(left=0.15) # prevent y-Label from being cut off
+  
   plt.savefig("plot.png")
   
 if __name__ == "__main__":