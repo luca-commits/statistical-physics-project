33fe338d8b03517c24b9afed308a0324dfaea08f
wolfartl <wolfartl@student.ethz.ch>
2020-05-12 17:41:58 +0200

    finish header file ChainInteractionCalculator.h

--- mdatom/source_code/lib/ChainInteractionCalculator.h
+++ mdatom/source_code/lib/ChainInteractionCalculator.h
@@ -10,18 +10,46 @@
  */
 
 class ChainInteractionCalculator : public InteractionCalculator{
+
     public:
       explicit ChainInteractionCalculator(const MDParameters& parameters);  //constructor
       
-      void calculateInteraction (int i, int j, const std::vector<double>& positions
-                                 std::vector<double>& forces, const std::vector<std::pair>&  bonds);
-      
+      //loops over all atoms and adds angle contributions to E_pot by calling calculateInteractionA
+      void calculateA (const std::vector<double>& positions, const std::vector<std::pair> bonds,
+                                const std::vector<double>& forces);
+    private:
+
       void calculateAngle(int i, int j, int l, const std::vector<double>& positions,
                           const std::vector<std::pair>& bonds);
-
       void calculateDihedral (int i, int j, int k, int l, const std::vector<double>& positions,
-                          const std::vector<std::pair>& bonds); 
-    
+                              const std::vector<std::pair>& bonds); 
+      //loops over all atoms and adds angle contributions to E_pot by calling calculateInteractionA
+      void calculateA (const std::vector<double>& positions, const std::vector<std::pair> bonds,
+                                const std::vector<double>& forces);
+      void calculateInteractionA (int i, int k, int j, const std::vector<double>& positions
+                                 std::vector<double>& forces, const std::vector<std::pair>&  bonds);
+      void calculatePotentialA();
+      void calculatePotentialAndForceMagnitude override();
+
+      void resetVariablesToZero override();
+      void initializeVariables override();
+
+
+      double theta0;
+      double gamma;
+
+      double dihedral_ij;
+      double angle_ijk;
+
+      ChainSimulationType type;
+};
+
+
+
+      
+
+
+      
 
 
 
